3885beb953be7e419fb0762e17f284da9bb76fa3
user8885 <user8885@realemail.com>
2025-08-23 05:21:59 -0400

    jk

--- hyprland/restart-hypr-portal.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-killall xdg-desktop-portal-hyprland
-
-exec /usr/lib/xdg-desktop-portal-hyprland
-
-
--- hyprland/showkeys.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sed -n '/START_KEYS/,/END_KEYS/p' ~/nix/home-manager/modules/hyprland.nix | less
-
